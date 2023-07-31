320d0a39f3ab59a9a597fccb0e1af3d0e967e225
Steven Chiu <schiu01@gmail.com>
2023-07-30 21:22:27 -0400

    updated to 2019 version

---
+++ Assets/scripts/EventManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public delegate void BallDeath();
+    public event BallDeath onBallDeath;
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+}
